861ccf28679d7d66c457b94ab3ab9861e76290bc
Stefano Zanella <zanella.stefano@gmail.com>
2013-01-10 18:50:50 +0100

    Add HTML frontend to the monitoring engine.
    Setup an EventMachine WebSocket server.
    Add a page that displays engine's output using HighCharts and WebSockets.

--- lib/metric_fetcher.rb
+++ lib/metric_fetcher.rb
@@ -8,7 +8,7 @@ class MetricFetcher
 
   def fetch(short_interval = '5min', long_interval = '24h', format = 'json')
     short_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=transformNull(#{@metric},0)&from=-#{short_interval}&format=#{format}").get
-    long_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=summarize(transformNull(#{@metric},0),\"300s\",\"avg\")&from=-#{long_interval}&format=#{format}").get
+    long_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=transformNull(summarize(transformNull(#{@metric},0),\"300s\",\"avg\"),0)&from=-#{long_interval}&format=#{format}").get
 
     multi = EM::MultiRequest.new
     multi.add :short, short_term_request
