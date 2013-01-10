93c6c5eb4a6d1054115bcd2d301f5882e024bbd1
Stefano Zanella <zanella.stefano@gmail.com>
2013-01-10 16:31:41 +0100

    Move MetricFetcher to own class file. Extract method on output.

---
+++ lib/metric_fetcher.rb
@@ -0,0 +1,25 @@
+class MetricFetcher
+  include EM::Deferrable
+
+  def initialize(host, metric)
+    @host = host
+    @metric = metric
+  end
+
+  def fetch(short_interval = '5min', long_interval = '24h', format = 'json')
+    short_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=transformNull(#{@metric},0)&from=-#{short_interval}&format=#{format}").get
+    long_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=summarize(transformNull(#{@metric},0),\"300s\",\"avg\")&from=-#{long_interval}&format=#{format}").get
+
+    multi = EM::MultiRequest.new
+    multi.add :short, short_term_request
+    multi.add :long, long_term_request
+    multi.callback do 
+      sample = {
+              :long_term => JSON.parse(long_term_request.response)[0]["datapoints"].map { |point| point[0] },
+              :short_term => JSON.parse(short_term_request.response)[0]["datapoints"].map { |point| point[0] } }
+      self.succeed(sample)
+    end
+  end
+end
+
+
