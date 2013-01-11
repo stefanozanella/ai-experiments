134a806d5557fcb09752b3a3f15595f055db4def
Stefano Zanella <zanella.stefano@gmail.com>
2013-01-11 14:53:46 +0100

    Add draft script to classify batches of samples to build training data.

--- lib/metric_fetcher.rb
+++ lib/metric_fetcher.rb
@@ -6,9 +6,14 @@ class MetricFetcher
     @metric = metric
   end
 
-  def fetch(short_interval = '5min', long_interval = '24h', format = 'json')
-    short_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=transformNull(#{@metric},0)&from=-#{short_interval}&format=#{format}").get
-    long_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=transformNull(summarize(transformNull(#{@metric},0),\"300s\",\"avg\"),0)&from=-#{long_interval}&format=#{format}").get
+  def fetch(opts = {})
+    short_interval = opts[:short_interval] || '5min'
+    long_interval  = opts[:long_interval]  || '24h'
+    up_to          = opts[:up_to]          || 'now'
+    format         = opts[:format]         || 'json'
+
+    short_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=transformNull(#{@metric},0)&from=#{up_to}-#{short_interval}&until=#{up_to}&format=#{format}").get
+    long_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=transformNull(summarize(transformNull(#{@metric},0),\"300s\",\"avg\"),0)&from=#{up_to}-#{long_interval}&until=#{up_to}&format=#{format}").get
 
     multi = EM::MultiRequest.new
     multi.add :short, short_term_request
