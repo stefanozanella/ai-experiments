adc033fdc7cfcb1f242eb0d37631a60700272953
Stefano Zanella <zanella.stefano@gmail.com>
2013-01-12 19:59:56 +0100

    Add mid term metric fetching. Storing metrics for training crashes.

--- lib/metric_fetcher.rb
+++ lib/metric_fetcher.rb
@@ -8,19 +8,23 @@ class MetricFetcher
 
   def fetch(opts = {})
     short_interval = opts[:short_interval] || '5min'
+    mid_interval   = opts[:mid_interval]  || '1h'
     long_interval  = opts[:long_interval]  || '24h'
     up_to          = opts[:up_to]          || 'now'
     format         = opts[:format]         || 'json'
 
     short_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=transformNull(#{@metric},0)&from=#{up_to}-#{short_interval}&until=#{up_to}&format=#{format}").get
+    mid_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=transformNull(summarize(transformNull(#{@metric},0),\"60s\",\"avg\"),0)&from=#{up_to}-#{mid_interval}&until=#{up_to}&format=#{format}").get
     long_term_request = EM::HttpRequest.new("http://#{@host}/render/?target=transformNull(summarize(transformNull(#{@metric},0),\"300s\",\"avg\"),0)&from=#{up_to}-#{long_interval}&until=#{up_to}&format=#{format}").get
 
     multi = EM::MultiRequest.new
     multi.add :short, short_term_request
+    multi.add :mid,   mid_term_request
     multi.add :long, long_term_request
     multi.callback do 
       sample = {
               :long_term => JSON.parse(long_term_request.response)[0]["datapoints"].map { |point| point[0] },
+              :mid_term => JSON.parse(mid_term_request.response)[0]["datapoints"].map { |point| point[0] },
               :short_term => JSON.parse(short_term_request.response)[0]["datapoints"].map { |point| point[0] } }
       self.succeed(sample)
     end
